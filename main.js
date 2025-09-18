dcc996d57117b9b1c5656f9313064d080647af4a
ANGÉL <asa@ANGEL.local>
2025-09-18 14:30:19 -0400

    updating js

--- main.js
+++ main.js
@@ -18,12 +18,12 @@ function addSubtract(bet){
   }
 document.querySelector('span.coins').innerText = coins
 
-let images = [ 'Dango.png', 'Cake.png', 'Chocolate.png' ]
-let img1 = Math.floor(Math.random()*3)
+let images = [ 'Dango.png', 'Cake.png', 'Chocolate.png', 'Coffee.png', 'Tea.png' ]
+let img1 = Math.floor(Math.random()*images.length)
 document.querySelector('.reel1 img').src=images [img1]
-let img2 = Math.floor(Math.random()*3)
+let img2 = Math.floor(Math.random()*images.length)
 document.querySelector('.reel2 img').src=images [img2]
-let img3 = Math.floor(Math.random()*3)
+let img3 = Math.floor(Math.random()*images.length)
 document.querySelector('.reel3 img').src=images [img3]
 
 if(img1 == img2 && img3 == img2){
